6fddec983d93927ff4237761cef41a9ac158f7e4
Triston117 <roviiknaris@yahoo.com>
2023-05-16 15:26:41 -0700

    Ugh

--- public/app.js
+++ public/app.js
@@ -1,4 +1,3 @@
-// app.js
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = document.getElementById("task-list");
   const dayOfWeekSelect = document.getElementById("day-of-week");
@@ -12,6 +11,8 @@ document.addEventListener("DOMContentLoaded", () => {
         tasks.forEach((task) => {
           const listItem = document.createElement("li");
           listItem.classList.add(day);
+          listItem.dataset.taskId = task.id; // Set the task ID as a data attribute
+
           listItem.innerHTML = `
             <input type="checkbox" id="${task.id}" name="task-${
             task.id
@@ -19,6 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <label for="${task.id}">${task.task}</label>
             <button>Delete</button>
           `;
+
           taskList.appendChild(listItem);
         });
       })
@@ -47,14 +49,14 @@ document.addEventListener("DOMContentLoaded", () => {
     taskInput.value = "";
 
     // Send POST request to add the task
-    fetch("/tasks", {
+    fetch(`/tasks/${dayOfWeek}`, {
+      // Update the fetch request URL
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
         task: taskInput.value,
-        day: dayOfWeek,
         completed: false,
       }),
     })
@@ -70,10 +72,11 @@ document.addEventListener("DOMContentLoaded", () => {
   function deleteTask(event) {
     if (event.target.tagName === "BUTTON") {
       const listItem = event.target.parentNode;
-      const taskId = listItem.querySelector("input[type=checkbox]").id;
+      const taskId = listItem.dataset.taskId; // Retrieve the task ID from the data attribute
 
       // Send DELETE request to remove the task
-      fetch(`/tasks/${taskId}`, {
+      fetch(`/tasks/${dayOfWeek}/${taskId}`, {
+        // Update the fetch request URL
         method: "DELETE",
       })
         .then((response) => response.json())
